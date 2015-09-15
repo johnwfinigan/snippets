5e338f2e095466d7d335ebe70b717f9e1896430f
John Finigan <johnwfinigan@gmail.com>
2015-09-15 13:46:29 -0500

    vmware set serial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # snippets
+
+* vmware-workstation-fusion-set-serial.sh - sets vmware workstation / fusion serial from the cli
---
+++ vmware-workstation-fusion-set-serial.sh
@@ -0,0 +1,3 @@
+#executable is in /Applications/VMware\ Fusion.app/Contents/Library on Mac OS X
+
+vmware-vmx --new-sn 11111-22222-NOT11-REAL22-SN333
