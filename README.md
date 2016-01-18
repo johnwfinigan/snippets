7dd237e6b3284cc3d6e0bbc723e0faa7b64f6ab5
John Finigan <johnwfinigan@gmail.com>
2016-01-18 06:15:48 -0600

    add xenserver dhcp reservation commands

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * sunray-mirror-screens.sh - mirrors screens for dual head SunRay clients
 * vmware-workstation-linux-unattended-install.sh - install vmware workstation in a shell script, unattended
 * centos7-nmcli-add-bridge.sh - add basic bridge interface on centos / rhel 7
+* xenserver-uuids-to-dhcp-reservations.sh - Use if you're cloning a bunch of VMs on xenserver and want to make ISC DHCPD reservations for them
---
+++ xenserver-uuids-to-dhcp-reservations.sh
@@ -0,0 +1,16 @@
+#These commands will take the UUIDs of xenserver VMs and output ISC DHCPD config file lines to 
+#give them DHCP reservations.
+#File example-uuids should contain the UUIDs of the VMs you want to make DHCP reservations for.
+#
+#One UUID per line. Intended to use the output of xe vm-copy or xe vm-clone
+#
+#Note to anti-useless-use-of-cat types: Think of cat as an operator to make
+#pipelines read left-to-right. 
+
+#Note: selects first virtual NIC and gets its MAC only
+cat example-uuid |  awk '{print "xe vif-list device=0  params=MAC vm-uuid=" $1}' | sh | awk '{print $5}' | sed  '/^$/d' > example-mac
+
+#Note: N is the starting IP's last byte (assumes 192.168.1.0/24 here). 203 is not special, just as sample.
+#exampleprefix is the host name prefix to be combined with N for a placeholder hostname
+#If you are using something aside from 192.168.1.0/24, fix this. 
+awk 'BEGIN {N=203} {print "host exampleprefix" N " { hardware ethernet " $1 "; fixed-address 192.168.1." N "; option host-name \"exampleprefix" N "\"; }" ; N=N+1;  }' < example-mac
